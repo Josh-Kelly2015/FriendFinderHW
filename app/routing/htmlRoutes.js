e4c00ec1e07289ac57dab76d254702335f195a93
Joshua Kelly <jdkelly2015@gmail.com>
2019-08-28 11:36:09 -0400

    attempting to create routes on html and api files

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,3 +1,25 @@
 // app.get(/survey)
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// route to survey
+app.get("/app/public/survey.html", function (req, res) {
+    res.sendFile(path.join(__dirname, "survey.html"));
+});
 
 //default catch all route to home.html
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+//listen method
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
