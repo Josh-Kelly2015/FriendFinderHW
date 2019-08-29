9dd5a3cfc6c50516d469b736b2999c9fca3b0562
Joshua Kelly <jdkelly2015@gmail.com>
2019-08-29 16:10:16 -0400

    error on post function inside survery.html $.post is not a function.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
         res.json(friends);
     });
-
+    console.log(friends);
     app.post("/api/friends", function (req, res) {
 
     })
