de52fd745e83651c852474aa49b9a93f6e27060b
Joshua Kelly <jdkelly2015@gmail.com>
2019-08-29 23:56:33 -0400

    my API friends list link will not display. Cannot GET pathway

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,13 @@
 var friends = require("../data/friends");
-require("path");
+var path = require("path");
 //app.get(/api/friends) to dispaly json
 module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
         res.json(friends);
     });
-    console.log(friends);
+  
     app.post("/api/friends", function (req, res) {
         // this is where i will parse my user data and apply js logic to compare and find the best match for the user
     })
-}
+};
 
